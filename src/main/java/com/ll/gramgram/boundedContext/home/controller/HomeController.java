931fe51da56b476e744b2d8d60b2af55def94539
udonggi <dongki1882@naver.com>
2023-05-01 14:57:06 +0900

    design: 앱 정보 추가

--- src/main/java/com/ll/gramgram/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/ll/gramgram/boundedContext/home/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.boundedContext.home.controller;
 
+import com.ll.gramgram.base.rq.Rq;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,9 +13,19 @@ import java.util.Enumeration;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
+    private final Rq rq;
+
     @GetMapping("/")
     public String showMain() {
-        return "usr/home/main";
+        if (rq.isLogout()) return "redirect:/usr/member/login";
+
+        return "redirect:/usr/member/me";
+    }
+
+    @GetMapping("/usr/home/about")
+    public String showAbout() {
+        return "usr/home/about";
     }
 
     @GetMapping("/usr/debugSession")
