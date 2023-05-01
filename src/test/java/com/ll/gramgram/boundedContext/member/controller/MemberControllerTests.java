52e5b18cc110b60e5e235f6af53d06a8bad9c716
udonggi <dongki1882@naver.com>
2023-05-01 15:10:18 +0900

    chore: 회원가입 폼 아예 삭제

--- src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
@@ -36,124 +36,18 @@ public class MemberControllerTests {
     @Autowired
     private MemberService memberService;
 
-    @Test
-    @DisplayName("회원가입 폼")
-    void t001() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(get("/usr/member/join"))
-                .andDo(print()); // 크게 의미 없고, 그냥 확인용
 
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("showJoin"))
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(containsString("""
-                        <input type="text" name="username"
-                        """.stripIndent().trim())))
-                .andExpect(content().string(containsString("""
-                        <input type="password" name="password"
-                        """.stripIndent().trim())))
-                .andExpect(content().string(containsString("""
-                        <input type="submit" value="회원가입"
-                        """.stripIndent().trim())));
-    }
 
-    @Test
-    // @Rollback(value = false) // DB에 흔적이 남는다.
-    @DisplayName("회원가입")
-    void t002() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10")
-                        .param("password", "1234")
-                        .param("email", "")
-                )
-                .andDo(print());
 
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrlPattern("/usr/member/login?msg=**"));
 
-        Member member = memberService.findByUsername("user10").orElse(null);
 
-        assertThat(member).isNotNull();
-    }
-
-    @Test
-    @DisplayName("회원가입시에 올바른 데이터를 넘기지 않으면 400")
-    void t003() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10")
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is4xxClientError());
-
-        // WHEN
-        resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("password", "1234")
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is4xxClientError());
-
-        // WHEN
-        resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10" + "a".repeat(30))
-                        .param("password", "1234")
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is4xxClientError());
-
-        // WHEN
-        resultActions = mvc
-                .perform(post("/usr/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10")
-                        .param("password", "1234" + "a".repeat(30))
-                )
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is4xxClientError());
-    }
 
     @Test
     @DisplayName("로그인 폼")
     void t004() throws Exception {
         // WHEN
         ResultActions resultActions = mvc
-                .perform(get("/usr/member/login"))
+                .perform(get("/usr/member/login?admin"))
                 .andDo(print());
 
         // THEN
@@ -166,9 +60,6 @@ public class MemberControllerTests {
                         """.stripIndent().trim())))
                 .andExpect(content().string(containsString("""
                         <input type="password" name="password"
-                        """.stripIndent().trim())))
-                .andExpect(content().string(containsString("""
-                        <input type="submit" value="로그인"
                         """.stripIndent().trim())));
     }
 
@@ -199,25 +90,6 @@ public class MemberControllerTests {
                 .andExpect(redirectedUrlPattern("/**"));
     }
 
-    @Test
-    // @Rollback(value = false) // DB에 흔적이 남는다.
-    @DisplayName("로그인 후에 내정보 클릭시 아이디 입력하라 메세지 나온다.")
-    @WithUserDetails("user1")
-        // user1로 로그인 한 상태로 진행
-    void t006() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(get("/usr/member/me"))
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("showMe"))
-                .andExpect(status().is4xxClientError())
-        ;
-
-    }
 
     @Test
     @DisplayName("findLoginId  테스트")
